b9ae5106755b4c8ddd12d9e5a2e87148bf9d1dde
bishal189 <bishalmurmu150@gmail.com>
2023-09-14 07:56:44 +0545

    created sign up pages

--- websiteproject/myaccount/views.py
+++ websiteproject/myaccount/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
+from .forms import ResitrationForm
+from.models import Account
+from django.contrib import messages,auth
+# Create your views here.    
+def Register(request):
+    if request.method == 'POST':
+        form =ResitrationForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']   
+            email=form.cleaned_data['email']  
+            password=form.cleaned_data['password']  
+            username=email.split("@")[0]
 
-# Create your views here.
+            user=Account.objects.create_user(email=email,username=username,password=password )
+            user.save()
+
+
+
+
+            # user activation
+            # current_site=get_current_site(request)
+            # mail_subject='Please activate your account'
+            # message=render_to_string('accounts/account_verfication_email.html',{
+            #     'user':user,
+            #     'domain':current_site,
+            #     'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+            #     'token':default_token_generator.make_token(user),
+            # })
+            # to_email=email
+            # send_email=EmailMessage(mail_subject,message,to=[to_email])
+            # send_email.send()
+
+
+
+
+
+
+
+            
+            messages.success(request,'Registration successful')
+            return redirect('register')
+
+
+
+
+    else:
+       form=ResitrationForm()
+    context={ 
+        'form':form
+    }
+    return render(request,'signup.html',context)
--- websiteproject/websiteproject/settings.py
+++ websiteproject/websiteproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myaccount',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'websiteproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,22 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+# this is for the static file configurations
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
+
+
+
+# for django messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+   
+}
