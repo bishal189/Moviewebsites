eab2e58344e949cd7530bf601127af17cd849895
sandeshsitaula <navaltajohan@gmail.com>
2023-09-17 11:32:04 +0545

    handle multiple file uploads and multiple genres

--- websiteproject/indexapp/admin.py
+++ websiteproject/indexapp/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import *
-
-admin.site.register(MovieDetail)
-admin.site.register(MovieCategory)
+from .forms import MovieDetailForm
+class MovieDetailAdmin(admin.ModelAdmin):
+    form=MovieDetailForm
+admin.site.register(ImagesModel)
+admin.site.register(MovieDetail,MovieDetailAdmin)
