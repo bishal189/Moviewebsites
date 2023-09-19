b1548215cabf75ab9aa3711c9c940bb43583bad8
bishal189 <bishalmurmu150@gmail.com>
2023-09-19 14:59:27 +0545

    added paginator

--- websiteproject/indexapp/views.py
+++ websiteproject/indexapp/views.py
@@ -1,15 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import MovieDetail
+from indexapp.models import Category
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 # Create your views here.
 
 
 def home(request):
-    get_data=MovieDetail.objects.all()
+   
+    all_product=MovieDetail.objects.all().order_by('id')
+    paginator=Paginator(all_product,1)
+    page=request.GET.get('page')
+    paged_products=paginator.get_page(page)
+    count=all_product.count()
+    # count1=paged_products.count()
     context={
-        'get_data':get_data
+        'all_products':paged_products,
+        'get_data':all_product,
+        'count':count,
+        
     }
     return render(request,'index.html',context)
-
+    
 
 def search(request):
     return render(request,'search.html')
+
+
+
+def pagination(request):
+    all_product=MovieDetail.objects.all().order_by('id')
+    paginator=Paginator(all_product,1)
+    page=request.GET.get('page')
+    paged_products=paginator.get_page(page)
+
+    count=all_product.count()
+    context={
+        'all_products':paged_products,
+        'count':count,
+        
+    }
+    return render(request,'index.html',context)
