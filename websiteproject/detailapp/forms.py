cffb7501dff04b2900b96fe64002b3a2e1ae8ac3
bishal189 <bishalmurmu150@gmail.com>
2023-10-07 10:30:05 +0545

    added fields in checkout

--- websiteproject/detailapp/forms.py
+++ websiteproject/detailapp/forms.py
@@ -3,7 +3,7 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model=Order
-        fields=['first_name','last_name','phone','email','address_line_1','address_line_2','country','state','city','order_note']
+        fields=['first_name','last_name','phone','email','address_line_1','zip_code','country','state','city','order_note','company_name']
         widgets = {
           'order_note': forms.Textarea(attrs={'rows':4, 'cols':15}),
         }
@@ -15,11 +15,12 @@ class OrderForm(forms.ModelForm):
         self.fields ['email'].widget.attrs['placeholder']='Enter email Address'      
         self.fields ['phone'].widget.attrs['placeholder']='Enter phone number'      
         self.fields ['address_line_1'].widget.attrs['placeholder']='Enter address_line_1'      
-        self.fields ['address_line_2'].widget.attrs['placeholder']='Enter address_line_2(optional) '      
+        self.fields ['zip_code'].widget.attrs['placeholder']='Enter Zip code'      
         self.fields ['country'].widget.attrs['placeholder']='Enter country name'      
         self.fields ['state'].widget.attrs['placeholder']='Enter state'      
         self.fields ['city'].widget.attrs['placeholder']='Enter city'      
         self.fields ['order_note'].widget.attrs['placeholder']='Enter order note'      
+        self.fields ['company_name'].widget.attrs['placeholder']='Enter company name'      
         
         for field in self.fields:
             self.fields [field].widget.attrs['class']='form-control'
