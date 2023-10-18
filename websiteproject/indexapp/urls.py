00abe2710bdecba6eea0643e5a720725ebf25053
sandeshsitaula <navaltajohan@gmail.com>
2023-10-18 19:41:12 +0545

    adding trans in static text

--- websiteproject/indexapp/urls.py
+++ websiteproject/indexapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls  import path
 from . import views
+from django.utils.translation import gettext as _
 urlpatterns = [
     path('',views.home,name='home'),
     path('search/',views.search,name='search'),
@@ -9,8 +10,8 @@ urlpatterns = [
     path('remove_favourites/<int:id>',views.remove_favourites,name='remove_favourites'),
     path('pagination/',views.pagination,name='pagination'),
     path('scenes/',views.scenes,name='scenes'),
-    path('DVD/',views.dvd,name='dvd'),
-    path('stars/',views.stars,name='stars'),
+    path(_('DVD/'),views.dvd,name='dvd'),
+    path(_('stars/'), views.stars, name='stars'),
     path('stars/<int:id>',views.star_detail,name='star_detail'),
     path('studio/<int:id>',views.studio_detail,name='studio_detail'),
     path('photosets/',views.photosets,name='photosets'),
